7cf089ede2752633a79dd6980974a712ed5bb995
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-04 17:22:19 -0500

    Create 4.-Update-datos.sql

---
+++ 4.-Update-datos.sql
@@ -0,0 +1,8 @@
+-- UPDATE
+UPDATE nombre_tabla SET nombre_campo_1 = <valor_cambio_1>, nombre_campo_2 = <valor_cambio_2 > WHERE nombre_campo_key = <valor_condición>;
+
+-- ACTUALIZAR TODOS LOS DATOS DE UN CAMPO
+UPDATE nombre_tabla SET nombre_campo = <valor_cambio>;
+
+
+ 
