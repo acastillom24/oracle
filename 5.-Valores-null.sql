003b9d998821d15d023298b185f2cb7c0fc179e5
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-04 17:45:14 -0500

    Create 5.-Valores-null.sql

---
+++ 5.-Valores-null.sql
@@ -0,0 +1,10 @@
+-- CONSULTAS NULL / IS NOT NULL
+
+SELECT * FROM nombre_table WHERE nombre_campo IS NULL;
+SELECT * FROM nombre_table WHERE nombre_campo IS NOT NULL;
+
+-- VALORES VACÍOS
+
+SELECT * FROM nombre_table WHERE nombre_campo '';
+SELECT * FROM nombre_table WHERE nombre_campo ' ';
+
