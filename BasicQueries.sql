1dc5b1e1ca997b249d5927225dbe620c2645d579
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-04 16:55:20 -0500

    Update BasicQueries.sql

--- BasicQueries.sql
+++ BasicQueries.sql
@@ -1,5 +1,30 @@
--- CREATE TABLE
-CREATE TABLE table_name(col1_name data_type,col2_name data_type,..,colN_name data_type);
+-- VISUALIZAR TODAS LAS TABLAS
+SELECT * FROM ALL_TABLES;
+
+-- CREAR TABLAS
+CREATE TABLE usuario(
+  id_usuario int,
+  nombre char(10),
+  fecha_nacimiento date,
+  salario number(6,2)
+);
+
+-- INFORMACIÓN DE LAS TABLAS
+DESCRIB nombre_tabla;
+
+-- INSERTAR REGISTROS EN UNA TABLA
+INSERT INTO nombre_tabla VALUES(
+  0001,
+  'Alin',
+  TO_DATE('24/08/1995', 'dd/mm/yyyy'),
+  453.04
+);
+
+
+
+
+
+
 
 
 
