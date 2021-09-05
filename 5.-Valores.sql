eca4d88e759124fecec93977d334fc97aa1a0084
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-05 17:10:13 -0500

    Update and rename 5.-Valores-null.sql to 5.-Valores.sql

--- 5.-Valores.sql
+++ 5.-Valores.sql
@@ -8,3 +8,7 @@ SELECT * FROM nombre_table WHERE nombre_campo IS NOT NULL;
 SELECT * FROM nombre_table WHERE nombre_campo '';
 SELECT * FROM nombre_table WHERE nombre_campo ' ';
 
+-- VALORES POR DEFECTO
+
+
+
