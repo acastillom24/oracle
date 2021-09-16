a2bc31a0ce036f8954595ccd488afcd535817f09
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-15 23:29:17 -0500

    Create 8.-Funciones-matematicas.sql

---
+++ 8.-Funciones-matematicas.sql
@@ -0,0 +1,26 @@
+-- funcion abs
+select abs(-200) from dual;
+
+-- funcion ceil
+select ceil(15.45) from dual;
+
+-- funcion floor
+select floor(15.95) from dual;
+
+-- funcion mod
+select mod(10, 3) from dual;
+
+-- funcion power
+select power(2, 3) from dual;
+
+-- funcion round
+select round(3.756, 1) from dual;
+
+-- funcion sing
+select sing(-100) from dual;
+
+-- funcion trunc
+select trunc(3.756, 2) from dual;
+
+-- funcion sqrt
+select round(sqrt(9)) from dual;
