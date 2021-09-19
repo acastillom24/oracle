bf9c5f4d93d8f262ac9734ec26074359699bdd26
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-09-19 00:32:54 -0500

    Update 0.-New-User.sql

--- 0.-New-User.sql
+++ 0.-New-User.sql
@@ -1,1 +1,16 @@
+-- Abrimos sql plus e ingresamos con el siguiente usuario:
+SYS /AS SYSDBA
 
+-- Crear un nuevo usuario.
+CREATE USER <nombre_usuario> IDENTIFIED BY <password>;
+
+-- Error: ORA-65096
+-- Comando: DCL - Lenguaje de control de datos.
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- Ejecutamos nuevamente el siguiente comando:
+CREATE USER <nombre_usuario> IDENTIFIED BY <password>;
+
+-- Asignar permisos y privilegios.
+-- Comando: DCL - Lenguaje de control de datos
+GRANT DBA TO <nombre_usuario>;
